d41acc803f131dfa6f17628c4ae00788c4c097a0
Harshal <harshaldharmik099@gmail.com>
2022-08-21 11:24:39 +0530

    Removed unused dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 import VueClipboard from 'vue-clipboard2';
 
 VueClipboard.config.autoSetContainer = true; // add this line
@@ -9,7 +8,5 @@ import { Quasar } from 'quasar';
 import quasarUserOptions from './quasar-user-options';
 const app = createApp(App).use(Quasar, quasarUserOptions);
 app.use(VueClipboard);
-
-app.use(createPinia());
 
 app.mount('#app');
